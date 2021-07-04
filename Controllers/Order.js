ae1b468bbb6faf804e2970ce8d97d12b135a6541
Erick Lumunge <lumungep12@gmail.com>
2021-07-04 09:36:42 +0300

    getting and creating orders

--- Controllers/Order.js
+++ Controllers/Order.js
@@ -1,6 +1,13 @@
 import Order from "../Models/Order.js";
 
-export const getOrders = async (req, res) => {};
+export const getOrders = async (req, res) => {
+	try {
+		const orders = await Order.find();
+		res.status(200).json(orders);
+	} catch (error) {
+		res.status(404).json({ message: error.message });
+	}
+};
 
 export const createOrder = async (req, res) => {
 	const order = req.body;
