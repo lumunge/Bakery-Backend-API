6ccd400c35da7d10f7dafaadc1390f586da3d55c
Erick Lumunge <lumungep12@gmail.com>
2021-08-28 01:47:11 +0300

    contact message

--- Models/Contact.js
+++ Models/Contact.js
@@ -14,6 +14,10 @@ const ContactSchema = mongoose.Schema(
 			type: String,
 			required: true,
 		},
+        message: {
+            type: String,
+            required: true,
+        },
 	},
 	{
 		timestamps: true,
